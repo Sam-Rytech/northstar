6030ea2c23a896dcbcac70460d146cf76c581959
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-20 22:56:41 +0100

    updated components import

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import Navbar from '@/components/Navbar'
-import Hero from '@/components/Hero'
-import NewArrivals from '@/components/NewArrivals'
-import Features from '@/components/Features'
-import PromoBanners from '@/components/PromoBanners'
-import TopSellers from '@/components/TopSellers'
-import Footer from '@/components/Footer'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import NewArrivals from '../components/NewArrivals'
+import Features from '../components/Features'
+import PromoBanners from '../components/PromoBanners'
+import TopSellers from '../components/TopSellers'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
