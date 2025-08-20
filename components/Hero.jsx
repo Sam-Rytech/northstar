674c244e43c3ec89fd0105e5ee76b0b90fb6ebb7
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-21 00:56:28 +0100

    Adjustment of hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,11 @@
 export default function Hero() {
   return (
     <section
-      className="relative h-[500px] flex items-center justify-center bg-cover bg-center"
+      className="relative h-[600px] flex items-center justify-end bg-cover bg-center"
       style={{ backgroundImage: "url('/Hero.png')" }}
     >
       <div className="text-center text-white">
-        <h1 className="text-4xl font-bold">STYLIST PICKS BEAT THE HEAT</h1>
+        <h1 className="text-5xl font-bold pr-20">STYLIST PICKS BEAT <br/> THE HEAT</h1>
         <button className="mt-4 px-6 py-2 border border-white">Shop Now</button>
       </div>
     </section>
