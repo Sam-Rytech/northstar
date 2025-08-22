3cb10ba03b226c6be9528ca637634dca247bc91a
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-22 03:31:54 +0100

    Updated image URL

--- components/about/Showcase.jsx
+++ components/about/Showcase.jsx
@@ -1,7 +1,7 @@
 export default function Showcase() {
   const items = [
-    { img: '/woman.jpg', btn: 'BUY NOW' },
-    { img: '/man.jpg', btn: 'BUY NOW' },
+    { img: '/About/Woman.png', btn: 'BUY NOW' },
+    { img: '/About/Man.png', btn: 'BUY NOW' },
   ]
 
   return (
@@ -11,7 +11,7 @@ export default function Showcase() {
           <img
             src={item.img}
             alt=""
-            className="w-full h-96 object-cover rounded-lg"
+            className="w-full object-cover rounded-lg"
           />
           <button className="absolute bottom-6 left-1/2 -translate-x-1/2 bg-white px-6 py-2 rounded-full shadow">
             {item.btn}
