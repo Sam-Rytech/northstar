453eba24e414b2274c63dc56a3d7d4b4c4d508d3
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-22 02:59:47 +0100

    Code for Hero section

---
+++ components/about/Hero.jsx
@@ -0,0 +1,10 @@
+export default function Hero() {
+  return (
+    <section
+      className="h-64 flex items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/about-hero.jpg')" }}
+    >
+      <h1 className="text-white text-4xl font-bold">ABOUT NORTHSTAR</h1>
+    </section>
+  )
+}
