de79076f0719f2ec5872817300ba281e72c0f422
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-22 03:26:49 +0100

    Updated the image URL

--- components/about/Hero.jsx
+++ components/about/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero() {
   return (
     <section
-      className="h-64 flex items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/about-hero.jpg')" }}
+      className="h-[350px] flex items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/About/Hero.png')" }}
     >
       <h1 className="text-white text-4xl font-bold">ABOUT NORTHSTAR</h1>
     </section>
