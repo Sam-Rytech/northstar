d82dc1c6718f81e5f2b4b0fbe8c7a8d55966b4ab
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-11 02:18:57 +0100

    Updated contact info

---
+++ components/contact/ContactInfo.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ContactInfo = () => {
+  return (
+    <div>
+      <h4 className="text-xl font-semibold mb-2">Visit Us</h4>
+      <p className="mb-6">
+        UET Lahore, Punjab, Pakistan <br />
+        Phone: +923039898987
+      </p>
+
+      <h4 className="text-xl font-semibold mb-2">Get In Touch</h4>
+      <p>
+        You can get in touch with us on this provided email. <br />
+        <span className="text-blue-600">hmjawad087@gmail.com</span>
+      </p>
+    </div>
+  )
+}
+
+export default ContactInfo
