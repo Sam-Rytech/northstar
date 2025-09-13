2ebb517c2d3aca733bd2a75ad09c465f5bfab567
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-09-13 22:51:26 +0100

    updated import statement

--- app/Checkout/page.jsx
+++ app/Checkout/page.jsx
@@ -1,4 +1,4 @@
-import Header from '@/components/Header'
+import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import CheckoutForm from '@/components/CheckoutForm'
 import CheckoutOrderSummary from '@/components/CheckoutOrderSummary'
@@ -17,7 +17,7 @@ export default function CheckoutPage() {
 
   return (
     <div>
-      <Header />
+      <Navbar />
 
       <main className="max-w-5xl mx-auto px-6 py-12 grid grid-cols-1 lg:grid-cols-2 gap-12">
         {/* Left side: Billing details */}
